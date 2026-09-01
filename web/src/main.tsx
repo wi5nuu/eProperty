0413e2bfb36a669505d264177328c541892555d3
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-09-01 21:34:45 +0700

    feat: enhance main entry point with ErrorBoundary and app initialization

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,14 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { ErrorBoundary } from './components/ErrorBoundary'
+import { useInitializeApp } from './hooks/useInitializeApp'
 import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
-    </BrowserRouter>
-  </StrictMode>
-)
+function Root() {
+  useInitializeApp()
+
+  return (
+    <StrictMode>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <App />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: '#363636',
+                color: '#fff',
+              },
+              success: {
+                duration: 3000,
+                iconTheme: {
+                  primary: '#10b981',
+                  secondary: '#fff',
+                },
+              },
+              error: {
+                duration: 4000,
+                iconTheme: {
+                  primary: '#ef4444',
+                  secondary: '#fff',
+                },
+              },
+            }}
+          />
+        </BrowserRouter>
+      </ErrorBoundary>
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<Root />)
